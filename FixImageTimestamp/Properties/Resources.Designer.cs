9fa81014333a30a35c14acba2adb63c178489040
Pham Phuong Bac <somethingnotright123@outlook.com>
2022-10-28 20:57:46 +0700

    Fix project .NET version

--- FixImageTimestamp/Properties/Resources.Designer.cs
+++ FixImageTimestamp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FixImageTimestamp.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -70,7 +70,7 @@ namespace FixImageTimestamp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 20170214 
+        ///   Looks up a localized string similar to 180721-Release 
         ///.
         /// </summary>
         internal static string BuildTime {
